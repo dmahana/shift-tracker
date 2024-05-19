26872676c83baa051aa8458ee18d1733be2bfa00
Deepak Mahana <92561418+dmahana@users.noreply.github.com>
2024-05-19 20:52:06 +1000

    input key validation (#1)
    * add input key validation
    
    * update layout
    
    * add margin to button
    
    * change get data button layout
    
    * update keys function

--- src/main.js
+++ src/main.js
@@ -4,55 +4,55 @@ const supbaseUrl = "https://vnkmsolriwnyfumugehk.supabase.co";
 const supabaseApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZua21zb2xyaXdueWZ1bXVnZWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwMTkwNzAsImV4cCI6MjAzMTU5NTA3MH0.9CV9KEzuGaWs6yQ44M8HkhVMMjJCmPa0YY_gK3cLV8A";
 const supabaseClient = supabase.createClient(supbaseUrl, supabaseApiKey);
 
-async function fnLogin() {
-    let login = false;
-    const userNameElement = document.getElementById("username");
-    const passwordElement = document.getElementById("password");
-    const responseElement = document.getElementById("response");
-
+async function getValidKeys() {
+    let result = []
     const { data, error } = await supabaseClient.from('userkeys').select();
-    userName = userNameElement.value;
-    password = passwordElement.value;
-    if (error) {
-        responseElement.innerText = "Failed to read user keys";
-    }
 
+    if (error) {
+        return result.push("mahana");
+    }
     for (let i = 0; i < data.length; i++) {
         _element = data[i];
-        if (userName === _element.user_name & password === _element.user_key) {
-            login = true;
-            document.cookie = "userName=" + userName;
-        }
+        result.push(_element.user_key);
     }
+
+    return result;
 }
 
-function fnLogout() {
-    mainContainer.style.display = "none";
-}
+
 
 let one_hour = 1000 * 60 * 60;
 
 async function addSupabaseItem() {
+    const validKeys = await getValidKeys();
     let addItemResultElement = document.getElementById("addItemResult");
+    let keyElement = document.getElementById("key");
 
-    let start_time_value = document.getElementById("start_time").value;
-    let end_time_value = document.getElementById("end_time").value;
+    if (!validKeys.includes(keyElement.value)) {
+        addItemResultElement.innerText = "Invalid Key!!!!";
 
-    let date = start_time_value.split("T")[0];
-
-    let startTime = Date.parse(start_time_value);
-    let endTime = Date.parse(end_time_value);
-
-    let hours = (endTime - startTime) / one_hour;
-    let start_time = start_time_value.split("T")[1];
-    let end_time = end_time_value.split("T")[1];
-
-    const { status, error } = await supabaseClient.from('shiftTracker').insert({ date: date, start_time: start_time, end_time: end_time, hours: hours });
-    if (status == 201) {
-        addItemResultElement.innerText = "Date : " + date + " | Start Time: " + start_time + " | End Time : " + end_time + " | Hours : " + hours;
     } else {
-        console.log(error)
-        addItemResultElement.innerText = "Unable to add item | " + error.message;
+
+        let start_time_value = document.getElementById("start_time").value;
+        let end_time_value = document.getElementById("end_time").value;
+
+        let date = start_time_value.split("T")[0];
+
+        let startTime = Date.parse(start_time_value);
+        let endTime = Date.parse(end_time_value);
+
+        let hours = (endTime - startTime) / one_hour;
+        let start_time = start_time_value.split("T")[1];
+        let end_time = end_time_value.split("T")[1];
+
+        const { status, error } = await supabaseClient.from('shiftTracker').insert({ date: date, start_time: start_time, end_time: end_time, hours: hours });
+        if (status == 201) {
+            addItemResultElement.innerText = "Date : " + date + " | Start Time: " + start_time + " | End Time : " + end_time + " | Hours : " + hours;
+        } else {
+            console.log(error)
+            addItemResultElement.innerText = "Unable to add item | " + error.message;
+        }
+
     }
 }
 
